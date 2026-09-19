8b04f7e22a473866a16daf638256b35cdc81afdd
phhgsi <ajay24ce008@satiengg.in>
2026-09-19 10:33:18 +0530

    fix(app): Add missing Bitmap import in PreviousStudentEditActivity

--- app/src/main/java/com/asacademy/schoolapp/PreviousStudentEditActivity.java
+++ app/src/main/java/com/asacademy/schoolapp/PreviousStudentEditActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
